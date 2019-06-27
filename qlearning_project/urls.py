a8daf735ff1108dde29ad2c7d07c481535bdc777
Alejandro <alerodri1994@gmail.com>
2019-06-27 20:04:58 +0200

    Add static to URL

--- qlearning_project/urls.py
+++ qlearning_project/urls.py
@@ -15,5 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("qlearning_app.urls"))]
+from . import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("qlearning_app.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
